14778645b70aaa41e386bfb49799623f32dec122
Maksym Kochut <feelofgoodvibes@gmail.com>
2022-10-01 07:03:45 +0300

    add all GET collections to crud.py and all GET collections endpoints to API

--- app.py
+++ app.py
@@ -17,13 +17,38 @@ def get_db():
         db.close()
 
 
-@app.get("/users/", response_model=list[schemas.UserSchema])
-def get_users(db: Session = Depends(get_db)):
-    users = crud.get_users(db)
-    return users
+@app.get("/countries/", response_model=list[schemas.Country])
+def get_countries(db: Session = Depends(get_db)):
+    countries = crud.get_countries(db)
+    return countries
 
 
 @app.get("/genres/", response_model=list[schemas.Genre])
 def get_genres(db: Session = Depends(get_db)):
     genres = crud.get_genres(db)
     return genres
+
+
+@app.get("/authors/", response_model=list[schemas.AuthorSchema])
+def get_genres(db: Session = Depends(get_db)):
+    authors = crud.get_authors(db)
+    return authors
+
+
+@app.get("/books/", response_model=list[schemas.BookSchema])
+def get_books(db: Session = Depends(get_db)):
+    books = crud.get_books(db)
+    return books
+
+
+
+@app.get("/tickets/", response_model=list[schemas.TicketSchema])
+def get_tickets(db: Session = Depends(get_db)):
+    tickets = crud.get_tickets(db)
+    return tickets
+
+
+@app.get("/users/", response_model=list[schemas.UserSchema])
+def get_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return users
--- crud.py
+++ crud.py
@@ -1,16 +1,42 @@
 from sqlalchemy.orm import Session
 import schemas
-from models import Genre, User, Country
+from models import Genre, User, Country, Author, Book, Ticket
 
 
 def get_genres(db: Session):
     return db.query(Genre).all()
 
+
 def get_countries(db: Session):
     return db.query(Country).all()
 
+
+def get_author(db: Session, author_id: int):
+    return db.query(Author).filter(Author.id == author_id).first()
+
+
+def get_authors(db: Session):
+    return db.query(Author).all()
+
+
+def get_book(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
+
+
+def get_books(db: Session):
+    return db.query(Book).all()
+
+def get_ticket(db: Session, ticket_id: int):
+    return db.query(Ticket).filter(Ticket.id == ticket_id).first()
+
+
+def get_tickets(db: Session):
+    return db.query(Ticket).all()
+
+
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
+
 
 def get_users(db: Session):
     return db.query(User).all()
